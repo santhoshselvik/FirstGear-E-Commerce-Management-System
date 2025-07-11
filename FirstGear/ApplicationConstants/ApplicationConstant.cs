ed6bf94105c1b1985ff0d6b8965ef8a266c49950
Santhosh Kumar <santhoshselvi.k01@gmail.com>
2025-07-11 14:28:28 +0530

    Add files via upload

---
+++ FirstGear/ApplicationConstants/ApplicationConstant.cs
@@ -0,0 +1,12 @@
+﻿namespace FirstGear.ApplicationConstants
+{
+    public class ApplicationConstant
+    {
+    }
+    public class CommonMessage
+    {
+        public static string RecordCreated= "Record Created Successfully";
+        public static string RecordUpdated = "Record Updated Successfully";
+        public static string RecordDeleted = "Record Deleted Successfully";
+    }
+}
